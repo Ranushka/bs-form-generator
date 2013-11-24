6b58d8d03a92fc57247dc4118d61d91685dd628f
Ranushka <ranushka543@gmail.com>
2013-11-24 09:13:38 -0800

    add delete functionality

--- dist/js/custamize_form.js
+++ dist/js/custamize_form.js
@@ -25,58 +25,69 @@ $(document).on('keyup', '.form_feld_id', function() {
 
 
 
-/*
-$(document).on('keyup', '.form_radio', function() {
-  // $(this).parent().siblings().find('input').attr('placeholder', $(this).val())
-  // var line = Math.floor((eventData.offsetY + scrollPosition) / lineHeight);
-  	// console.log($(this).val().split("\n"))
-// <label><input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">Option one</label>
-// <label><input type="radio">Option one</label>
-  	// $(this).val().split("\n").each($.find('.radio').append('<label><input type="radio">Option one</label>'));
-  	
-  	// var new_checkbox_n_value = $(this).val().split("\n");
-  	// $.each(new_checkbox_n_value,function () {
-  	// 	console.log(new_checkbox_n_value)
-  	// 	$(this).find('.radio').append('<label><input type="radio">Option one</label>')
-  	// })
 
-             console.log($(this).parent().siblings().find('.radio').html());
-             // $(this).parent().siblings().find('.radio').empty();
+$(document).on('keydown', '.form_radio', function(e)
+{
 
-	var lines = $(this).val().split("\n");
+	// console.log(e.keyCode);
+	// if(e.keyCode == 13)
+	// {
+		$(event.target).parent().siblings().find('.radio_gp').empty();
+        var lines = $(this).val().split("\n");
+	    $.each(lines, function(n, elem) {
+	             // console.log(n);
+	    	$(event.target).parent().siblings().find('.radio_gp').append('<div class="radio">'+
+				'<label>'+
+					'<input type="radio" name="optionsRadios" id="'+n+'" value="'+elem+'">'+
+					'<span>'+elem+'</span>'+
+				'</label>'+
+			'</div>');
+		}); 
+    // }		
+});
+
+
+$(document).on('keydown', '.form_checkbox', function(e)
+{
+	$(event.target).parent().siblings().find('.checkbox_gp').empty();
+    var lines = $(this).val().split("\n");
+    $.each(lines, function(n, elem) {
+    	$(event.target).parent().siblings().find('.checkbox_gp').append('<div class="checkbox">'+
+			'<label>'+
+				'<input type="checkbox" name="optionsRadios" id="'+n+'" value="'+elem+'">'+
+				'<span>'+elem+'</span>'+
+			'</label>'+
+		'</div>');
+	});	
+});
+
+
+
+$(document).on('keydown', '.form_select', function(e)
+{
+	$(event.target).parent().siblings().find('select').empty();
+    var lines = $(this).val().split("\n");
     $.each(lines, function(n, elem) {
              // console.log(n);
-    	$(event.target).parent().siblings().find('.radio').append('<label><input type="radio">'+elem+'</label>')
-
+    	$(event.target).parent().siblings().find('select').append('<option>'+elem+'</option>');
 	});
 });
-*/
-
-
-		$(document).on('keydown', '.form_radio', function(e)
-		{
-
-			// console.log(e.keyCode);
-			// if(e.keyCode == 13)
-			// {
-				$(event.target).parent().siblings().find('.radio_gp').empty();
-	            var lines = $(this).val().split("\n");
-			    $.each(lines, function(n, elem) {
-			             // console.log(n);
-			    	$(event.target).parent().siblings().find('.radio_gp').append('<div class="radio">'+
-						'<label>'+
-							'<input type="radio" name="optionsRadios" id="'+n+'" value="'+elem+'">'+
-							'<span>'+elem+'</span>'+
-						'</label>'+
-					'</div>');
-				}); 
-	        // }		
-		});
 
 
 
 
-	});// end document ready
+
+
+
+
+
+
+
+
+
+
+
+});// end document ready
 
 
 
--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,7 +1,7 @@
 $(function() {
 		$( "#catalog" ).accordion();
 		
-		$( "#newForm" ).sortable();
+		
 
 
 		// 
@@ -11,6 +11,8 @@ $(function() {
 			appendTo: "body",
 			helper: "clone"
 		});
+
+		
 
 
 
@@ -23,7 +25,7 @@ $(function() {
 		{
 			activeClass: "new_Form_home_hilight",
 			hoverClass: "ui-state-hover",
-			accept: "#formElements .form-group, #help_text .help-block",
+			accept: "#formElements .form-group",
 
 			// accept: ":not(#help_text>span)",
 			drop: function( event, ui ) 
@@ -36,31 +38,59 @@ $(function() {
 				$(ui.draggable.closest('article').attr(
 					{
 						'data-toggle'	: 'collapse',
-						'data-parent'	: '#accordion',
+						'data-parent'	: '#newForm',
 						'href'			: '#collapse'+new_form_id
 					} )
 				)
 
-				var collapse_trigure = 'data-toggle="collapse" data-parent="#accordion" href="#collapse'+new_form_id+'" ';
+				// var collapse_trigure = 'data-toggle="collapse" data-parent="#accordion" href="#collapse'+new_form_id+'" ';
         		var editingConent = '<div id="collapse'+new_form_id+'" class="panel-collapse collapse editing_panal"> '+editing_conent_options+' </div>'
 
-				$( '<li></li>' ).append(ui.draggable.clone(),editingConent).appendTo( this );
+				$( '<li class="panel panel-default"></li>' ).append(ui.draggable.clone(),editingConent).appendTo( this );
+
+				// make_drop_to_dragable();
+				$( "#newForm" ).sortable();
 
 
-			},
-
-
+			}
 
 		});// end fun droppable
 
+		// 
+		// make dropabl elements to dragable
+		// 
+		function make_drop_to_dragable () {
+			$( "#newForm li.panel" ).draggable({
+				appendTo: "body",
+				helper: "clone"
+			});
+		}
 
 
+		// 
+		// make dropable to delete form fields & elements
+		// 
+		$( "#form_delete_element" ).droppable(
+		{
+			activeClass: "form_delete_element_hilight",
+			hoverClass: "ui-state-hover",
+			accept: "#newForm li.panel",
 
-			function get_li_count ()
+			// accept: ":not(#help_text>span)",
+			drop: function( event, ui ) 
 			{
-				var n = $( "#newForm li" ).size();
-				return n;
+				$( ui.draggable ).remove();
 			}
+
+		});// end fun droppable form_delete_element
+
+
+
+		function get_li_count ()
+		{
+			var n = $( "#newForm li" ).size();
+			return n;
+		}
 
 
 
@@ -140,7 +170,7 @@ $(function() {
 		// set_password_type_editing_conent_options
 		// 
 		function set_password_type_editing_conent_options () {
-			return get_label_edit() + get_placeholder_edit();
+			return get_label_edit() + get_placeholder_edit() + get_feld_name_edit() + get_feld_id_edit();
 		}
 
 
@@ -148,7 +178,7 @@ $(function() {
 		// set_textarea_type_editing_conent_options
 		// 
 		function set_textarea_type_editing_conent_options () {
-			return get_label_edit() + get_placeholder_edit();
+			return get_label_edit() + get_placeholder_edit() + get_feld_name_edit() + get_feld_id_edit();
 		}
 
 
@@ -156,7 +186,7 @@ $(function() {
 		// set_textarea_type_editing_conent_options
 		// 
 		function set_radio_type_editing_conent_options () {
-			return get_label_edit() + get_radio_edit();
+			return get_label_edit() + get_radio_edit() + get_feld_name_edit();
 		}
 
 
@@ -164,7 +194,7 @@ $(function() {
 		// set_textarea_type_editing_conent_options
 		// 
 		function set_checkbox_type_editing_conent_options () {
-			return get_label_edit() + get_placeholder_edit();
+			return get_label_edit() + get_checkbox_edit() + get_feld_name_edit();
 		}
 
 
@@ -172,7 +202,7 @@ $(function() {
 		// set_textarea_type_editing_conent_options
 		// 
 		function set_select_type_editing_conent_options () {
-			return get_label_edit() + get_placeholder_edit();
+			return get_label_edit() + get_select_edit() + get_feld_name_edit();
 		}
 
 
@@ -180,7 +210,7 @@ $(function() {
 		// set_textarea_type_editing_conent_options
 		// 
 		function set_select_multiple_type_editing_conent_options () {
-			return get_label_edit() + get_placeholder_edit();
+			return get_label_edit() + get_select_edit() + get_feld_name_edit();
 		}
 
 
@@ -217,8 +247,12 @@ $(function() {
 			return '<textarea class="form-control form_radio" rows="3"></textarea>';
 		}
 
-		function get_radio_checkbox_edit () {
-			return '<label><input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">Option one</label>';
+		function get_checkbox_edit () {
+			return '<textarea class="form-control form_checkbox" rows="3"></textarea>';
+		}
+
+		function get_select_edit () {
+			return '<textarea class="form-control form_select" rows="3"></textarea>';
 		}
 
 
@@ -253,7 +287,10 @@ $(function() {
 			alert the html
 			*/
 
-			$('#temp_form').html($('#newForm').html());
+			// $('#temp_form').html($('#newForm').html());
+			$('#temp_form').empty();
+
+			$('<form class="form-horizontal" role="form"></form>').append($('#newForm').html()).appendTo('#temp_form')
 
 			$('#temp_form .editing_panal').remove();
 
@@ -268,11 +305,15 @@ $(function() {
 			$('#temp_form article').removeAttr('data-toggle')
 			$('#temp_form article').removeAttr('data-parent')
 			$('#temp_form article').removeAttr('href')
+
+
 			
 
 			var generated_html = $('#temp_form').html();
 
-			$('#output_html .modal-body>textarea').html(generated_html);
+			$('#output_html .modal-body>pre').text(generated_html);
+			$('#temp_form').empty();
+			prettyPrint();
 			$('#output_html').modal('show');
 			// alert(generated_html);
 		})
